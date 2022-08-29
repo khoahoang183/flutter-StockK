7f8ac5bece6de7b79e35f22fa83e4bc17f27a40a
Khoa Hoang Dang <khoa@Khoas-MacBook-Air.local>
2022-08-29 21:35:46 +0700

    Steve - Update project

--- lib/ui/home/HomeProductListTitle.dart
+++ lib/ui/home/HomeProductListTitle.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+import '../../resources/ResourceImage.dart';
+
+class HomeProductListTile extends StatelessWidget {
+  const HomeProductListTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SizedBox(
+      width: double.infinity,
+      height: 100,
+      child: Row(
+        children: [
+          Image.asset(width: 80, height: 80, "${ResourceImages.AssetsPrefix}img_home_ads_1.png", fit: BoxFit.cover),
+          const Text("dat dsa dsa dsadasa")
+        ],
+      ),
+    ));
+  }
+}
--- lib/ui/home/HomeScreenUI.dart
+++ lib/ui/home/HomeScreenUI.dart
@@ -10,3 +10,4 @@ class HomeScreenUI extends StatefulWidget {
     return HomeScreenState();
   }
 }
+
