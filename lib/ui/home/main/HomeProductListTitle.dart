d44ba7163a339d30e047f44bd65c4e6c02fa50c1
hoangdangkhoam9 <hoangdangkhoa.m9@gmail.com>
2022-08-29 17:59:43 +0700

    Steve - Update project

---
+++ lib/ui/home/main/HomeProductListTitle.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:stockk_flutter/model/ProductModel.dart';
+
+import '../../resources/ResourceImage.dart';
+
+class HomeCategoryListTitle extends StatelessWidget {
+  final ProductModel model;
+
+  // ignore: use_key_in_widget_constructors
+  const HomeCategoryListTitle({Key? key, required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      width: 120,
+      child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 5, 20, 5),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                "${ResourceImages.AssetsPrefix}img_home_ads_1.png",
+                fit: BoxFit.cover,
+                scale: 0.1,
+              ))),
+    );
+  }
+}
---
+++ lib/ui/home/main/HomeScreenMainUI.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:stockk_flutter/resources/ResourceDimens.dart';
+
+import '../../model/ProductModel.dart';
+import '../../resources/ResourceImage.dart';
+import '../../resources/ResourceStrings.dart';
+import '../../util/view/system/SysAppBar.dart';
+import 'HomeProductListTitle.dart';
+
+// UI StatefulWidget
+class HomeScreenMainUI extends StatefulWidget {
+  const HomeScreenMainUI({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeScreenState();
+  }
+}
+
+// State HomeScreenState
+class HomeScreenState extends State<HomeScreenMainUI> {
+  HomeScreenState();
+
+  int _currentPage = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+  final List<Widget> _lstPageView = [
+    Image.asset("${ResourceImages.AssetsPrefix}img_home_ads_1.png",
+        fit: BoxFit.cover),
+    Image.asset("${ResourceImages.AssetsPrefix}img_home_ads_2.png",
+        fit: BoxFit.cover),
+    Image.asset("${ResourceImages.AssetsPrefix}img_home_ads_3.png",
+        fit: BoxFit.cover),
+  ];
+
+  final List<ProductModel> _lstProduct = ProductModel().getDummyList();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Handle State of PageView ads
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      if (_currentPage == _lstPageView.length - 1) {
+        _currentPage = 0;
+      } else {
+        _currentPage++;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 1000),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: SysAppBar(),
+        body: RefreshIndicator(
+            onRefresh: () async {},
+            child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // PageView top ads
+                    SizedBox(
+                      height: 300,
+                      width: double.infinity,
+                      child: PageView(
+                          controller: _pageController, children: _lstPageView),
+                    ),
+                    buildProductCategoryWidget(),
+                  ],
+                ))));
+  }
+
+  // buildCategoryWidget
+  Widget buildProductCategoryWidget() {
+    return // Content
+      Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          ResourceStrings.home_screen_category_title,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: ResourceDimens.text_size_18, fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 120,
+                    child: ListView.builder(
+                        physics: const ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _lstProduct.length,
+                        itemBuilder: (context, index) {
+                          return HomeCategoryListTitle(
+                              model: _lstProduct[index]);
+                        }),
+                  )
+                ],
+              )));
+  }
+
+  // buildCategoryWidget
+  Widget buildProductGroupWidget() {
+    return // Content
+      Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          ResourceStrings.home_screen_category_title,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: ResourceDimens.text_size_18, fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 120,
+                    child: ListView.builder(
+                        physics: const ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _lstProduct.length,
+                        itemBuilder: (context, index) {
+                          return HomeCategoryListTitle(
+                              model: _lstProduct[index]);
+                        }),
+                  )
+                ],
+              )));
+  }
+}
